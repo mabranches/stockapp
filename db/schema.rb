50c3dae6789d551eb77c478693dec1bc7022c279
Manoel Rebelo Abranches <abranches@gmail.com>
2016-04-26 12:59:15 -0300

    Cria acesso de usuarios

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160425014228) do
+ActiveRecord::Schema.define(version: 20160425215422) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -29,6 +29,16 @@ ActiveRecord::Schema.define(version: 20160425014228) do
 
   add_index "items", ["sub_category_id"], name: "index_items_on_sub_category_id", using: :btree
 
+  create_table "stocks", force: :cascade do |t|
+    t.integer  "item_id",      limit: 4
+    t.integer  "warehouse_id", limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  add_index "stocks", ["item_id"], name: "index_stocks_on_item_id", using: :btree
+  add_index "stocks", ["warehouse_id"], name: "index_stocks_on_warehouse_id", using: :btree
+
   create_table "sub_categories", force: :cascade do |t|
     t.string   "name",        limit: 255
     t.integer  "category_id", limit: 4
@@ -38,6 +48,33 @@ ActiveRecord::Schema.define(version: 20160425014228) do
 
   add_index "sub_categories", ["category_id"], name: "index_sub_categories_on_category_id", using: :btree
 
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "warehouses", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.string   "address",    limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
   add_foreign_key "items", "sub_categories"
+  add_foreign_key "stocks", "items"
+  add_foreign_key "stocks", "warehouses"
   add_foreign_key "sub_categories", "categories"
 end
