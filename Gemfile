520a59f34437f88bd1e64e4373a5742c5a4a532f
Thiago Peçanha <thiago.pecanha@mot-br.com>
2016-05-05 18:06:17 -0300

    Adding Users Avatar

--- Gemfile
+++ Gemfile
@@ -35,6 +35,9 @@ gem 'devise'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+# PaperClip for attachment management
+gem 'paperclip', '~> 5.0.0.beta1'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,21 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+
   protect_from_forgery with: :exception
+
+  before_filter :configure_permitted_exception_parameters, if: :devise_controller?
+
+  protected
+  def configure_permitted_exception_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user|
+        user.permit(:name, :email, :password, :avatar)
+    end
+
+    devise_parameter_sanitizer.permit(:account_update) do |user|
+        user.permit(:name, :email, :password, :current_password, :avatar)
+    end
+
+end
+
 end
